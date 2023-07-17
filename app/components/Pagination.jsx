624571239d22dfebddb044ab2802226545461cb3
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 16:16:40 -0300

    Modify styles for pagination buttons

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -7,7 +7,7 @@ const Pagination = ({ currentPage, totalPages, paginate }) => {
       {currentPage > 1 && (
         <button
           onClick={() => paginate(currentPage - 1)}
-          className="mr-2 bg-green-950 text-white px-4 py-2 rounded"
+          className="mr-2 bg-white text-green-950 border border-green-950 hover:bg-green-950 hover: px-4 py-2 rounded hover:text-white hover:border-white"
         >
           Prev
         </button>
@@ -15,7 +15,7 @@ const Pagination = ({ currentPage, totalPages, paginate }) => {
       {currentPage < totalPages && (
         <button
           onClick={() => paginate(currentPage + 1)}
-          className="bg-green-950 text-white px-4 py-2 rounded"
+          className="bg-white text-green-950 border border-green-950 hover:bg-green-950 hover: px-4 py-2 rounded hover:text-white hover:border-white"
         >
           Next
         </button>
