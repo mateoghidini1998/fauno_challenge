308a214093ac26514f30323fa4d662686750f242
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 19:06:54 -0300

    Restructure file system

--- app/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export function Header() {
-    return (
-        <header className='w-full h-16 bg-white' >
-          <nav >
-            
-          </nav>
-        </header>
-    )
-}
--- app/posts/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Post({ params }){
-    const { id } = params;
-
-    return <>
-        <h1>This is a Post {id}</h1>
-    </>
-}
