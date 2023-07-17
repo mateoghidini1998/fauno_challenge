5fa366e0ef0b535a8bac475dd44aa54db829d706
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 19:05:53 -0300

    Create Hero Section

---
+++ app/components/Posts/PostsHero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PostsHero() {
+  return (
+    <div className="w-full bg-gradient-to-br from-customGreen to-customDarkTeal flex flex-col items-center justify-center mb-[70px] h-[200px] sm:h-[255px]">
+      <h1 className="text-[50px] sm:text-[70px] text-white">Home Posts</h1>
+      <h4 className="text-white text-[20px] sm:text-[27px]">here you can find all posts</h4>
+    </div>
+  );
+}
+
+export default PostsHero;
