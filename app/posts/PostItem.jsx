9ea1bc82a039ce2bc9d9077d3f3b6849616ae5a9
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 14:52:25 -0300

    Create Edit Post  functionality in a different component

--- app/posts/PostItem.jsx
+++ app/posts/PostItem.jsx
@@ -1,14 +1,11 @@
 'use client'
 import React, { useState } from 'react'
-import { GiCancel } from 'react-icons/gi';
-import { RiSave3Fill } from 'react-icons/ri';
 import { GrEdit } from 'react-icons/gr';
+import EditPost from './EditPost';
 
 function PostItem({ post, updatePost }) {
 
     const [ isEditing, setIsEditing ] = useState(false);
-    const [updatedTitle, setUpdatedTitle] = useState(post.title);
-    const [updatedBody, setUpdatedBody] = useState(post.body);
     
     const handleEdit = (postId) => {
         setIsEditing(true);
@@ -19,39 +16,19 @@ function PostItem({ post, updatePost }) {
         
     }
 
-    const handleSave = (postId) => {
-        updatePost(postId, updatedTitle, updatedBody);
-        setIsEditing(false);
+    const handleSaveSuccess = () => {
+      setIsEditing(false);
     };
 
     return (
-      <article className="relative flex flex-col gap-2 border-9 border-black-1000 rounded-2xl bg-green-950 whitespace-normal lg:w-auto md:w-auto w-full max-w-md m-6 h-auto p-4 overflow-hidden shadow-3xl text-black">
+      <article className="relative flex flex-col gap-2 border-9 border-black-1000 rounded-2xl bg-green-950 whitespace-normal lg:w-auto md:w-auto w-full max-w-md m-6 h-auto p-4  shadow-3xl text-black">
         {isEditing ? (
-          <div>
-            <input
-              className="bg-transparent text-black border-b-2 rounded-sm border-black uppercase mb-4"
-              type="text"
-              value={updatedTitle}
-              onChange={(e) => setUpdatedTitle(e.target.value)}
-            />
-            <textarea
-              className="bg-transparent border-2 border-black p-2 text-black w-full h-auto whitespace-pre-wrap overflow-hidden rounded-lg"
-              value={updatedBody}
-              onChange={(e) => setUpdatedBody(e.target.value)}
-            />
-            <button
-              className="border-9 rounded-xl border-black-1000 bg-blue-600 text-black p-2 shadow-2xl w-10 absolute top-2 right-16"
-              onClick={() => handleSave(post.id)}
-            >
-              <RiSave3Fill className="w-full h-full" />
-            </button>
-            <button
-              className="border-9 rounded-xl border-black-1000 bg-red-700 text-black p-2 shadow-2xl w-10 absolute top-2 right-2"
-              onClick={() => handleCancelEdit(post.id)}
-            >
-              <GiCancel />
-            </button>
-          </div>
+          <EditPost
+          post={post}
+          updatePost={updatePost}
+          onCancelEdit={handleCancelEdit}
+          onSaveSuccess={handleSaveSuccess}
+          />
         ) : (
           <div className='w-auto'>
             <h2 className="text-xl font-bold text-white uppercase mb-4">
@@ -62,7 +39,7 @@ function PostItem({ post, updatePost }) {
               className="border-9 rounded-xl border-black-1000 bg-yellow-500 text-black p-2 shadow-2xl w-10 absolute bottom-2 right-2 m-2"
               onClick={() => handleEdit(post.id)}
             >
-              <GrEdit />
+              <GrEdit className='w-full  h-full' />
             </button>
           </div>
         )}
