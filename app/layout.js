88d1f619910d6812e9748ea524e8e0a3defbcb31
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 16:16:19 -0300

    Remove header from layout

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,12 @@
 import './globals.css';
-import { Header } from './components/Header';
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
-        <title>My first App</title>
+        <title>Posts App</title>
       </head>
-      <body className='bg-green-950 w-auto'>
-        <Header />
-        {children}
-      </body>
+      <body className='bg-green-950 w-auto'>{children}</body>
     </html>
   );
 }
