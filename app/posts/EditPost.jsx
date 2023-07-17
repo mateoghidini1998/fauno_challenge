f841cd7b323c351b69b01b719d037c7706555e20
mateoghidini1998 <mateghidini123@gmail.com>
2023-07-17 16:15:31 -0300

    Update styles for actions buttons

--- app/posts/EditPost.jsx
+++ app/posts/EditPost.jsx
@@ -16,30 +16,30 @@ function EditPost({post, updatePost, onCancelEdit, onSaveSuccess}) {
 
   return (
     <div className='h-auto'>
-            <input
-              className="bg-transparent text-black border-b-2 rounded-sm border-black uppercase mb-4"
-              type="text"
-              value={updatedTitle}
-              onChange={(e) => setUpdatedTitle(e.target.value)}
-            />
-            <textarea
-              className="bg-transparent border-2 border-black p-2 text-black w-full h-auto whitespace-pre-wrap overflow-hidden rounded-lg overflow-y-auto"
-              value={updatedBody}
-              onChange={(e) => setUpdatedBody(e.target.value)}
-            />
-            <button
-              className="border-9 rounded-xl border-black-1000 bg-blue-600 text-black p-2 shadow-2xl w-auto absolute top-2 right-16"
-              onClick={() => handleSave(post.id)}
-            >
-              <RiSave3Fill className="w-full h-full" />
-            </button>
-            <button
-              className="border-9 rounded-xl border-black-1000 bg-red-700 text-black p-2 shadow-2xl w-auto absolute top-2 right-2"
-              onClick={onCancelEdit}
-            >
-              <GiCancel className="w-full h-full" />
-            </button>
-          </div>
+        <input
+        className="bg-transparent text-black border-b-2 rounded-sm border-black uppercase mb-4"
+        type="text"
+        value={updatedTitle}
+        onChange={(e) => setUpdatedTitle(e.target.value)}
+        />
+        <textarea
+        className="bg-transparent border-2 border-black p-2 text-black w-full h-auto min-h-[115px] whitespace-pre-wrap  rounded-lg  overflow-hidden scrollable"
+        value={updatedBody}
+        onChange={(e) => setUpdatedBody(e.target.value)}
+        />
+        <button
+        className="border-9 rounded-xl border-black-1000 bg-blue-600 text-black p-2 shadow-2xl w-auto  absolute top-2 right-16"
+        onClick={() => handleSave(post.id)}
+        >
+        <RiSave3Fill className="w-full h-full" />
+        </button>
+        <button
+        className="border-9 rounded-xl border-black-1000 bg-red-700 text-black p-2 shadow-2xl w-auto absolute top-2 right-2"
+        onClick={onCancelEdit}
+        >
+        <GiCancel className="w-full h-full" />
+        </button>
+    </div>
   )
 }
 
